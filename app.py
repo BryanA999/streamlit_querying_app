15f075d04020c3a0af0fd723458804afcb9410fb
Richard T <77714442+CoffeeAddict93@users.noreply.github.com>
2022-06-21 09:35:04 -0500

    docstrings for functions

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 """
 Streamlit Web App for Python Querying System
 """
+
 # libraries
 
+# Title of the app
 
-# markdown short description goes here
+# Picture Header
 
+# markdown (include an emoji) short description goes here
 
-# extra info
+# any extra info goes here
 
 ###
 
-# TOD0: file upload 
+# TOD0: file upload
 # uploaded_files = st.file_uploader("Upload .txt files", accept_multiple_files=True)
 # for uploaded_file in uploaded_files:
-    # bytes_data = uploaded_file.read()
-    # st.write("filename:", uploaded_file.name)
-    # st.write(bytes_data)
-       
-###   
-   
+# bytes_data = uploaded_file.read()
+# st.write("filename:", uploaded_file.name)
+# st.write(bytes_data)
+
+###
+
 # TOD0: display uploaded files (?)
 # if so, display as either dictionary or dataframe (?)
 
@@ -30,10 +33,21 @@ Streamlit Web App for Python Querying System
 
 ###
 
-# TOD0: query function
+# TOD0: display query results
 # send files to query_backend for both calculations and ranking
-# return ranking results 
+# return ranking results
 # this could be as dictionary or dataframe (?)
+
+# form to communicate with backend
+# with st.form():
+# query = st.text_input("Enter query:")
+# submitted = st.form_submit("Submit Query")
+# if submitted:
+# with st.spinner("Loading..."):
+# results = query_backend.query_ranking(query)
+# with st.container()
+# st.write(results)
+
 
 ###
 
--- query_backend.py
+++ query_backend.py
@@ -1,1 +1,40 @@
+"""
+Backend for the Query System.
+"""
 
+# libraries for the backend
+
+
+# class query_backend:
+
+# def preprocess_files(self, files):
+# """
+# Takes .txt files,  tokenizes them, removes stopwords, and returns a list of lists.
+#
+# Args:
+#   files (UploadedFile objects): list of .txt files
+#
+# Returns:
+#   processed_files (list): list of lists of preprocessed files
+# """
+
+
+# def calculate_weights(self, data):
+# """
+# Takes preprocessed files from preprocess_files(), performs calculations, and returns dataframe of calculations.
+#
+# Args:
+#  data (list): list of lists of preprocessed files
+#
+# Returns:
+#   dataframe (pandas dataframe): dataframe of calculations
+# """
+
+
+# def query_ranking(self, dataframe, query):
+# """
+# Takes query and dataframe from calculate_term_weighting(), applies similarity function, and returns ranking results.
+#
+# Args:
+#   dataframe (pandas dataframe): dataframe of calculations
+#   query (str): query string
