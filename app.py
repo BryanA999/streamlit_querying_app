9679106b6a1d836ac5bae54217ee196782f46b78
Richard T <77714442+Coffee-93@users.noreply.github.com>
2022-06-20 21:47:04 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+"""
+Streamlit Web App for Python Querying System
+"""
+# libraries
+
+
+# markdown short description goes here
+
+
+# extra info
+
+###
+
+# TOD0: file upload 
+# uploaded_files = st.file_uploader("Upload .txt files", accept_multiple_files=True)
+# for uploaded_file in uploaded_files:
+    # bytes_data = uploaded_file.read()
+    # st.write("filename:", uploaded_file.name)
+    # st.write(bytes_data)
+       
+###   
+   
+# TOD0: display uploaded files (?)
+# if so, display as either dictionary or dataframe (?)
+
+
+###
+
+# some artificial divider goes here
+
+###
+
+# TOD0: query function
+# send files to query_backend for both calculations and ranking
+# return ranking results 
+# this could be as dictionary or dataframe (?)
+
+###
+
+# footnote goes here
